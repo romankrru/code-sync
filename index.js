46becd9b34fd8eb1096ccbb47d16a3619905ac10
romankrru <rm07ru@gmail.com>
2019-02-02 13:33:05 +0300

    -

--- index.js
+++ index.js
@@ -8,22 +8,21 @@ const homedir = require('os').homedir();
 
 const rl = require('readline').createInterface({
 	input: process.stdin,
-	output: process.stdout
+	output: process.stdout,
 });
 
 const getPathToSettings = () => {
 	const common = ['Code', 'User', 'settings.json'];
 
-	if(process.platform === "win32")
-		return path.join(homedir, 'AppData', 'Roaming', ...common);
+	if (process.platform === 'win32') return path.join(homedir, 'AppData', 'Roaming', ...common);
 
 	return path.join(homedir, '.config', ...common);
-}
+};
 
-const readInstalledExtensions = () => exec('code --list-extensions')
-
+const readInstalledExtensions = () =>
+	exec('code --list-extensions')
 		.then(({stdout, stderr}) => {
-			if(stderr) {
+			if (stderr) {
 				console.log(`Unable to get extensions. stderr: ${stderr}`);
 				process.exit(1);
 			}
@@ -33,9 +32,7 @@ const readInstalledExtensions = () => exec('code --list-extensions')
 
 		.catch(console.error);
 
-const transformExtensionsStringToArray = extensionsString => extensionsString
-	.split('\n')
-	.filter(Boolean); // exclude empty strings
+const transformExtensionsStringToArray = extensionsString => extensionsString.split('\n').filter(Boolean); // exclude empty strings
 
 const ask = question => new Promise(resolve => rl.question(question, answer => resolve(answer)));
 
@@ -49,7 +46,7 @@ const uninstallExtension = extension => {
 	return exec(`code --uninstall-extension ${extension}`);
 };
 
-const [,,command] = process.argv;
+const [, , command] = process.argv;
 
 const commands = {
 	'save-settings': () => {
@@ -65,7 +62,7 @@ const commands = {
 	'apply-settings': () => {
 		rl.close();
 
-		console.log('Applying settings...')
+		console.log('Applying settings...');
 
 		copyFile('./settings.json', getPathToSettings())
 			.then(() => console.log('Success!'))
@@ -82,71 +79,75 @@ const commands = {
 	},
 
 	'install-extensions': () => {
-		console.log('Installing extensions...')
-		console.log('---')
+		console.log('Installing extensions...');
+		console.log('---');
 
 		let extensionsToInstall = [];
 		let extensionsToUninstall = [];
 
 		readInstalledExtensions()
-
 			.then(installedExtensionsStr => {
 				const installedExtensions = transformExtensionsStringToArray(installedExtensionsStr);
-				const savedExtensions = transformExtensionsStringToArray(readFileSync('./extensions', {encoding: 'utf8'}));
+				const savedExtensions = transformExtensionsStringToArray(
+					readFileSync('./extensions', {encoding: 'utf8'}),
+				);
 
-				if(!savedExtensions.length) {
+				if (!savedExtensions.length) {
 					console.log('`extensions` file is empty');
 					process.exit(1);
 				}
 
 				extensionsToInstall = savedExtensions.reduce((acc, extensionName) => {
-					if(installedExtensions.includes(extensionName))
-						return acc;
+					if (installedExtensions.includes(extensionName)) return acc;
 
 					acc.push(extensionName);
 					return acc;
 				}, []);
 
 				extensionsToUninstall = installedExtensions.reduce((acc, extensionName) => {
-					if(savedExtensions.includes(extensionName))
-						return acc;
+					if (savedExtensions.includes(extensionName)) return acc;
 
 					acc.push(extensionName);
 					return acc;
 				}, []);
 
-				if(!extensionsToUninstall.length && !extensionsToInstall.length) {
+				if (!extensionsToUninstall.length && !extensionsToInstall.length) {
 					console.log('Nothing to install and uninstall');
 					process.exit(0);
 				}
 
-				return ask([
+				return ask(
+					[
 						extensionsToInstall.length
-							? `Extensions to install (${extensionsToInstall.length}):\n ${extensionsToInstall.join('\n')} \n`
+							? `Extensions to install (${extensionsToInstall.length}):\n ${extensionsToInstall.join(
+									'\n',
+							  )} \n`
 							: 'Nothing to install \n',
 
 						'---\n',
 
 						extensionsToUninstall.length
-							? `Extensions to uninstall (${extensionsToUninstall.length}):\n ${extensionsToUninstall.join('\n')} \n`
+							? `Extensions to uninstall (${
+									extensionsToUninstall.length
+							  }):\n ${extensionsToUninstall.join('\n')} \n`
 							: 'Nothing to uninstall \n',
 
 						'---\n',
 
-						'Are you sure you want to continue? (y/N): '
-				].join(''));
+						'Are you sure you want to continue? (y/N): ',
+					].join(''),
+				);
 			})
 
 			.then(answer => {
 				rl.close();
 
-				if(answer !== 'y') {
+				if (answer !== 'y') {
 					process.exit(0);
 				}
 
 				return Promise.all(extensionsToInstall.map(installExtension));
 			})
-
 
 			.then(() => Promise.all(extensionsToUninstall.map(uninstallExtension)))
 			.then(() => console.log('Success!'))
@@ -159,5 +160,5 @@ const commands = {
 	},
 };
 
-const run = (command in commands) ? commands[command] : commands.__unknown__;
+const run = command in commands ? commands[command] : commands.__unknown__;
 run();
